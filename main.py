8c268aeaaa84667eac161d48a6a789e13ef6aaf8
Jayden-Briffa <jaydenbriffa@outlook.com>
2026-02-20 09:06:30 +0000

    Implement validation

--- main.py
+++ main.py
@@ -5,8 +5,51 @@ current_tasks = dict()
 priority_idx = Priority_Queue()
 number_retrieved = 0
 
-# Id to be given to the nexdt task
+# Id to be given to the next task
 newest_id = 1
+
+def validate_task(description, priority):
+    messages = []
+
+    if not description:
+        messages.append("Field 'description' must be given")
+
+    if not priority:
+        messages.append("Field 'priority' must be given")
+
+    try:
+        priority = int(priority)
+        if priority < 0:
+            messages.append("Field 'priority' must be greater than, or equal to, 0")
+
+    except ValueError:
+        messages.append("Field 'priority' must be a number")
+
+    return messages
+
+# POINT: Returns list instead of bool for consistency in validation/errors
+def validate_id(id):
+    messages = []
+
+    try:
+        id = int(id)
+        found_task = get_task_by_id()
+        if not found_task:
+            messages.append(f"There is no task with the id '${id}'")
+
+    except ValueError:
+        messages.append("Field 'id' must be a number")
+
+    return messages, found_task
+
+# POINT: Emphasises error messages with trailing blank line and trailing ***
+def print_error_messages(messages):
+    print()
+
+    for msg in messages:
+        print(f"*** ERROR: ${msg} *** ")
+    
+    print()
 
 def load_tasks():
     # TODO: Change state management to class to improve readability
@@ -23,6 +66,7 @@ def load_tasks():
 
 
 def save_tasks():
+    print("Saving. Do not close the program...")
     with open("tasks.json") as f:
         json.dump({
             "current_tasks": current_tasks,
@@ -84,6 +128,11 @@ while True:
             description = input("Enter a description for the new task: ")
             priority = input("Enter a priority for the new task: ")
 
+            messages = validate_task(description, priority)
+            if messages:
+                print_error_messages(messages)
+                pass 
+
             new_task = add_task(description, int(priority))
             newest_id += 1
             print(new_task)
@@ -99,13 +148,21 @@ while True:
         case "3":
             id = input("Enter the task's id: ")
 
-            found_task = get_task_by_id(int(id))
+            messages, found_task = validate_id(id)
+            if messages:
+                print_error_messages(messages)
+                pass
 
             print(found_task)
 
         # Complete task
         case "4":
             id = input("Enter the task's id: ")
+
+            messages, _ = validate_id(id)
+            if messages:
+                print_error_messages(messages)
+                pass
 
             task = complete_task(int(id))
             print(task)
